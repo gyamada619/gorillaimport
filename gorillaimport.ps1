744c7173c191bb4a8337b1b702b8bc738dd1b201
Garrett Yamada <me@garrettyamada.com>
2018-11-20 19:09:52 -0600

    Added "-Force" parameter to better ensure content is added.

--- gorillaimport.ps1
+++ gorillaimport.ps1
@@ -17,7 +17,7 @@ $hashstring = $filehash.Hash
 
 #Compile user input and hash into YAML data and append it to designated catalog
 $yaml = ConvertTo-Yaml @{"$itemname"=@{"display_name"="$displayname"; "installer_item_location"="$pkgpath"; "installer_item_hash"="$hashstring"; "version"="$version"}}
-Add-Content -Path "catalogs/$catalog" -Value $yaml
+Add-Content -Path "catalogs/$catalog" -Value $yaml -Force
 
 #Let user know we're done
 Write-Output "Package has been imported sucessfully."
